6002ea001293e6818db5fef7404764b40029511b
Bruno Martins <brunoseivam@gmail.com>
2026-01-21 21:01:50 -0500

    Cleanup screens

--- iocBoot/ioc-lakeshore346/st.cmd
+++ iocBoot/ioc-lakeshore346/st.cmd
@@ -15,11 +15,11 @@ drvAsynIPPortConfigure("$(PORT)","localhost:7777")
 asynOctetSetInputEos("$(PORT)", 0, "\r\n")
 asynOctetSetOutputEos("$(PORT)", 0, "\r\n")
 
-#asynSetTraceIOMask("L0", -1, 0x2)
-#asynSetTraceMask("L0", -1, 0x9)
+asynSetTraceIOMask("L0", -1, 0x2)
+asynSetTraceMask("L0", -1, 0x9)
 
-lakeshore346_autoload_db("$(TOP)/db","$(PORT)",$(ADDR=0),"P=LS346:,R=,PORT=$(PORT),ADDR=0,SCAN=$(SCAN),TEMPSCAN=$(TEMPSCAN)")
-#dbLoadRecords("$(TOP)/db/lakeshore346_core.db", "P=LS346:,R=,PORT=$(PORT),ADDR=0,SCAN=$(SCAN),TEMPSCAN=$(TEMPSCAN)")
+#lakeshore346_autoload_db("$(TOP)/db","$(PORT)",$(ADDR=0),"P=LS346:,R=,PORT=$(PORT),ADDR=0,SCAN=$(SCAN),TEMPSCAN=$(TEMPSCAN)")
+dbLoadRecords("$(TOP)/db/lakeshore346_core.db", "P=LS346:,R=,PORT=$(PORT),ADDR=0,SCAN=$(SCAN),TEMPSCAN=$(TEMPSCAN)")
 
 
 iocInit()
