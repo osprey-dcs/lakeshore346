0d3edfc6000cea662475d06b009d37de68d39184
Omitto <diego.omitto@gmail.com>
2026-03-11 12:11:02 -0400

    Add reconnect logic, reboot button, and iocStats module

--- iocBoot/ioc-lakeshore346/st.cmd
+++ iocBoot/ioc-lakeshore346/st.cmd
@@ -11,14 +11,18 @@ epicsEnvSet("TEMPSCAN", "1")
 dbLoadDatabase("$(TOP)/dbd/lakeshore346.dbd")
 lakeshore346_registerRecordDeviceDriver(pdbbase)
 
-drvAsynIPPortConfigure("$(PORT)","localhost:7777")
+drvAsynIPPortConfigure("$(PORT)", "192.168.0.100:7777")
 asynOctetSetInputEos("$(PORT)", 0, "\r\n")
 asynOctetSetOutputEos("$(PORT)", 0, "\r\n")
+asynSetOption("$(PORT)", -1, "disconnectOnReadTimeout", "Y")
 
 #asynSetTraceIOMask("L0", -1, 0x2)
 #asynSetTraceMask("L0", -1, 0x9)
 
 lakeshore346_autoload_db("$(TOP)/db","$(PORT)",$(ADDR=0),"P=LS346:,R=,PORT=$(PORT),ADDR=0,SCAN=$(SCAN),TEMPSCAN=$(TEMPSCAN)")
 #dbLoadRecords("$(TOP)/db/lakeshore346_core.db", "P=LS346:,R=,PORT=$(PORT),ADDR=0,SCAN=$(SCAN),TEMPSCAN=$(TEMPSCAN)")
+
+dbLoadRecords("$(IOCSTATS)/db/ioc.db", "IOCNAME=LS346,TODFORMAT=%m/%d/%Y %H:%M:%S")
+dbLoadRecords("$(TOP)/db/lakeshore346_reconnect.db", "P=LS346:,R=,IOC=LS346")
 
 iocInit()
